ad4bfc3710d8f3d5d054a090fbbc557c4088ce25
tarjei <tarjei.skjaerset@gmail.com>
2021-12-22 23:03:19 +0100

    Add unit tests for all exported functions

--- tests/unit.test.ts
+++ tests/unit.test.ts
@@ -1,4 +1,25 @@
-import { decode, boolean, decodeType, number, string, tuple } from '../src';
+import {
+  decode,
+  boolean,
+  decodeType,
+  number,
+  string,
+  tuple,
+  literal,
+  record,
+  undef,
+  nil,
+  field,
+  fields,
+  union,
+  date,
+  optional,
+  array,
+  set,
+  map,
+  dict,
+  nullable,
+} from '../src';
 
 test('homogeneous tuple', () => {
   const t: [string, string] = ['a', 'b'];
@@ -52,4 +73,394 @@ test('nested tuple literal', () => {
   const tuple_decoder = decode([string, [string, string]]);
 
   expect<tuple>(tuple_decoder(t)).toEqual(t);
+});
+
+test('literal string', () => {
+  const l1: 'a' = 'a' as const;
+
+  type literal = decodeType<typeof literal_decoder>;
+  const literal_decoder = literal(l1);
+
+  expect<literal>(literal_decoder(l1)).toEqual(l1);
+  expect(() => literal_decoder('b')).toThrow();
+});
+
+// not supported for some reason
+// test('literal number', () => {
+//   const l1: 1 = 1 as const;
+
+//   type literal = decodeType<typeof literal_decoder>;
+//   const literal_decoder = literal(1);
+
+//   expect<literal>(literal_decoder(l1)).toEqual(l1);
+// });
+
+test('decode string', () => {
+  const l1: 'a' = 'a' as const;
+
+  type literal = decodeType<typeof literal_decoder>;
+  const literal_decoder = decode(l1);
+
+  expect<literal>(literal_decoder(l1)).toEqual(l1);
+  expect(() => literal_decoder('b')).toThrow();
+});
+
+test('decode record', () => {
+  const l1: {} = {} as const;
+
+  type literal = decodeType<typeof literal_decoder>;
+  const literal_decoder = decode({});
+
+  expect<literal>(literal_decoder(l1)).toEqual(l1);
+  expect(() => literal_decoder(null)).toThrow();
+});
+
+test('record decoder', () => {
+  const l1: {} = {} as const;
+
+  type record = decodeType<typeof record_decoder>;
+  const record_decoder = record({});
+
+  expect<record>(record_decoder(l1)).toEqual(l1);
+  expect(() => record_decoder(null)).toThrow();
+});
+
+test('record decoder with some data', () => {
+  const l1 = { str: 'hei', num: 85, bool: true, missing: null };
+
+  type record = decodeType<typeof record_decoder>;
+  const record_decoder = record({
+    str: string,
+    num: number,
+    bool: boolean,
+    missing: nil,
+  });
+
+  expect<record>(record_decoder(l1)).toEqual(l1);
+  expect(() => record_decoder({ str: 'hei' })).toThrow();
+});
+
+test('record decoder with nested literal data', () => {
+  const l1 = { str: 'hei', num: 85, rec: { str: 'dete', data: 'data' } };
+
+  type record = decodeType<typeof record_decoder>;
+  const record_decoder = record({
+    str: string,
+    num: number,
+    rec: {
+      str: string,
+      data: 'data',
+    },
+  });
+
+  expect<record>(record_decoder(l1)).toEqual(l1);
+  expect(() =>
+    record_decoder({
+      str: 'hei',
+      num: 85,
+      rec: { str: 'dete', data: 'wrong string' },
+    }),
+  ).toThrow();
+});
+
+test('field decoder', () => {
+  const data = 'data';
+  const l1 = { f: data };
+
+  type record = decodeType<typeof record_decoder>;
+  const record_decoder = record({
+    f: field('f', string),
+  });
+
+  expect<record>(record_decoder(l1)).toEqual(l1);
+  expect(() => record_decoder({ g: 'data' })).toThrow();
+});
+
+test('field decoder: rename', () => {
+  const data = 'data';
+  const l1 = { f: data };
+
+  type record = decodeType<typeof record_decoder>;
+  const record_decoder = record({
+    g: field('f', string),
+  });
+
+  expect<record>(record_decoder(l1)).toEqual({ g: data });
+  expect(() => record_decoder({ g: 'data' })).toThrow();
+});
+
+test('field decoder: nested', () => {
+  const data = 'data';
+  const l1 = { data: { f: data } };
+
+  type record = decodeType<typeof record_decoder>;
+  const record_decoder = record({
+    data: { f: field('f', string) },
+  });
+
+  expect<record>(record_decoder(l1)).toEqual(l1);
+  expect(() => record_decoder({ g: 'data' })).toThrow();
+});
+
+test('fields decoder', () => {
+  const data = 'data';
+  const l1 = { f: data, g: 0 };
+
+  type record = decodeType<typeof record_decoder>;
+  const record_decoder = record({
+    f: string,
+    g: number,
+    h: fields({ f: string, g: number }, ({ f, g }) => f + g),
+  });
+
+  const result = { ...l1, h: l1.f + l1.g };
+
+  expect<record>(record_decoder(l1)).toEqual(result);
+  expect<record>(
+    record_decoder({ ...l1, h: 'this key will be ignored' }),
+  ).toEqual(result);
+});
+
+test('union decoder', () => {
+  const l1 = 'test data';
+
+  type union = decodeType<typeof union_decoder>;
+  const union_decoder = union(string, number);
+
+  expect<union>(union_decoder(l1)).toEqual(l1);
+  expect<union>(union_decoder(34)).toEqual(34);
+  expect(() => union_decoder(true)).toThrow();
+});
+
+test('multi union decoder', () => {
+  const data = 'test data';
+
+  type union = decodeType<typeof union_decoder>;
+  const union_decoder = union(
+    string,
+    number,
+    boolean,
+    undef,
+    nil,
+    tuple(string, string),
+    record({ data: string }),
+  );
+
+  expect<union>(union_decoder(data)).toEqual(data);
+  expect<union>(union_decoder(34)).toEqual(34);
+  expect<union>(union_decoder(true)).toEqual(true);
+  expect<union>(union_decoder(undefined)).toEqual(undefined);
+  expect<union>(union_decoder(null)).toEqual(null);
+  expect<union>(union_decoder([data, data])).toEqual([data, data]);
+  expect<union>(union_decoder({ data })).toEqual({ data });
+  expect(() => union_decoder({ fail: '' })).toThrow();
+});
+
+test('union with default case pattern', () => {
+  const l1 = 'test data';
+
+  type union = decodeType<typeof union_decoder>;
+  const union_decoder = union(string, () => '');
+
+  expect<union>(union_decoder('')).toEqual('');
+  expect<union>(union_decoder(l1)).toEqual(l1);
+  expect<union>(union_decoder(null)).toEqual('');
+  expect<union>(union_decoder(false)).toEqual('');
+});
+
+test('optional string decoder', () => {
+  const l1 = 'test data';
+
+  type optional = decodeType<typeof optional_decoder>;
+  const optional_decoder = optional(string);
+
+  expect<optional>(optional_decoder('')).toEqual('');
+  expect<optional>(optional_decoder(l1)).toEqual(l1);
+  expect<optional>(optional_decoder(undefined)).toEqual(undefined);
+  expect(() => optional_decoder(null)).toThrow();
+
+  const data = { optional_decoder: l1 };
+  expect(record({ optional_decoder })(data)).toEqual(data);
+  expect(record({ optional_decoder })({})).toEqual({
+    optional_decoder: undefined,
+  });
+});
+
+test('array decoder', () => {
+  const l1 = 'test data';
+
+  type array = decodeType<typeof array_decoder>;
+  const array_decoder = array(string);
+
+  expect<array>(array_decoder([])).toEqual([]);
+  expect<array>(array_decoder([l1])).toEqual([l1]);
+  expect<array>(array_decoder([l1, ''])).toEqual([l1, '']);
+  expect(() => array_decoder('')).toThrow();
+  expect(() => array_decoder([l1, true])).toThrow();
+  expect(() => array_decoder([l1, undefined])).toThrow();
+  expect(() => array_decoder([[]])).toThrow();
+  expect(() => array_decoder(null)).toThrow();
+  expect(() => array_decoder({})).toThrow();
+});
+
+test('set decoder', () => {
+  const l1 = 'test data';
+
+  type set = decodeType<typeof set_decoder>;
+  const set_decoder = set(string);
+
+  expect<set>(set_decoder([])).toEqual(new Set());
+  expect<set>(set_decoder([l1])).toEqual(new Set([l1]));
+  expect<set>(set_decoder([l1, ''])).toEqual(new Set(['', l1]));
+  expect(() => set_decoder([l1, true])).toThrow();
+  expect(() => set_decoder([l1, undefined])).toThrow();
+  expect(() => set_decoder([[]])).toThrow();
+  expect(() => set_decoder(null)).toThrow();
+  expect(() => set_decoder({})).toThrow();
+});
+
+test('map (list-of-records) decoder', () => {
+  const l1 = [
+    { data: 'one', meta_data: 1 },
+    { data: 'two', meta_data: 2 },
+  ];
+
+  type map = decodeType<typeof map_decoder>;
+  const map_decoder = map(
+    { data: string, meta_data: number },
+    ({ data }) => data,
+  );
+
+  expect<map>(map_decoder([])).toEqual(new Map());
+  expect<map>(map_decoder(l1)).toEqual(new Map(l1.map((x) => [x.data, x])));
+});
+
+test('map (list-of-strings) decoder', () => {
+  const l1 = ['one thing', 'and another'];
+
+  type map = decodeType<typeof map_decoder>;
+  const map_decoder = map(string, (s) => s.length);
+
+  expect<map>(map_decoder([])).toEqual(new Map());
+  expect<map>(map_decoder(l1)).toEqual(new Map(l1.map((x) => [x.length, x])));
+});
+
+test('dict decoder', () => {
+  const l1 = { one: 1, two: 2 };
+
+  type dict = decodeType<typeof dict_decoder>;
+  const dict_decoder = dict(number);
+
+  expect<dict>(dict_decoder({})).toEqual(new Map());
+  expect<dict>(dict_decoder(l1)).toEqual(
+    new Map([
+      ['one', 1],
+      ['two', 2],
+    ]),
+  );
+});
+
+test('nullable decoder', () => {
+  type nullable = decodeType<typeof nullable_decoder>;
+  const nullable_decoder = nullable(boolean);
+
+  expect<nullable>(nullable_decoder(true)).toEqual(true);
+  expect<nullable>(nullable_decoder(false)).toEqual(false);
+  expect<nullable>(nullable_decoder(null)).toEqual(null);
+  expect(() => nullable_decoder(undefined)).toThrow();
+  expect(() => nullable_decoder('')).toThrow();
+  expect(() => nullable_decoder([])).toThrow();
+  expect(() => nullable_decoder({})).toThrow();
+});
+
+test('string decoder', () => {
+  type primitve_type = decodeType<typeof decoder>;
+  const decoder = string;
+
+  expect<primitve_type>(decoder('')).toEqual('');
+  expect<primitve_type>(decoder('test data')).toEqual('test data');
+  expect(() => decoder(0)).toThrow();
+  expect(() => decoder(false)).toThrow();
+  expect(() => decoder(undefined)).toThrow();
+  expect(() => decoder(null)).toThrow();
+  expect(() => decoder([])).toThrow();
+  expect(() => decoder({})).toThrow();
+});
+
+test('number decoder', () => {
+  type primitve_type = decodeType<typeof decoder>;
+  const decoder = number;
+
+  expect<primitve_type>(decoder(0)).toEqual(0);
+  expect<primitve_type>(decoder(1)).toEqual(1);
+  expect<primitve_type>(decoder(-1)).toEqual(-1);
+  expect<primitve_type>(decoder(Infinity)).toEqual(Infinity);
+  expect<primitve_type>(decoder(-Infinity)).toEqual(-Infinity);
+  expect<primitve_type>(decoder(NaN)).toEqual(NaN);
+  expect(() => decoder('')).toThrow();
+  expect(() => decoder('0')).toThrow();
+  expect(() => decoder('1')).toThrow();
+  expect(() => decoder(false)).toThrow();
+  expect(() => decoder(undefined)).toThrow();
+  expect(() => decoder(null)).toThrow();
+  expect(() => decoder([])).toThrow();
+  expect(() => decoder({})).toThrow();
+});
+
+test('boolean decoder', () => {
+  type primitve_type = decodeType<typeof decoder>;
+  const decoder = boolean;
+
+  expect<primitve_type>(decoder(true)).toEqual(true);
+  expect<primitve_type>(decoder(false)).toEqual(false);
+  expect(() => decoder('')).toThrow();
+  expect(() => decoder(0)).toThrow();
+  expect(() => decoder(undefined)).toThrow();
+  expect(() => decoder(null)).toThrow();
+  expect(() => decoder([])).toThrow();
+  expect(() => decoder({})).toThrow();
+});
+
+test('undefined decoder', () => {
+  type primitve_type = decodeType<typeof decoder>;
+  const decoder = undef;
+
+  expect<primitve_type>(decoder(undefined)).toEqual(undefined);
+  expect(() => decoder('')).toThrow();
+  expect(() => decoder(0)).toThrow();
+  expect(() => decoder(true)).toThrow();
+  expect(() => decoder(null)).toThrow();
+  expect(() => decoder([])).toThrow();
+  expect(() => decoder({})).toThrow();
+});
+
+test('null decoder', () => {
+  type primitve_type = decodeType<typeof decoder>;
+  const decoder = nil;
+
+  expect<primitve_type>(decoder(null)).toEqual(null);
+  expect(() => decoder('')).toThrow();
+  expect(() => decoder(0)).toThrow();
+  expect(() => decoder(true)).toThrow();
+  expect(() => decoder(undefined)).toThrow();
+  expect(() => decoder([])).toThrow();
+  expect(() => decoder({})).toThrow();
+});
+
+test('date decoder', () => {
+  type primitve_type = decodeType<typeof decoder>;
+  const decoder = date;
+
+  expect<primitve_type>(decoder('2020-02-20')).toEqual(new Date('2020-02-20'));
+  expect<primitve_type>(decoder('2020-02-20T23:59')).toEqual(
+    new Date('2020-02-20T23:59'),
+  );
+  expect<primitve_type>(decoder('2222')).toEqual(new Date('2222-01-01'));
+  expect(() => decoder('')).toThrow();
+  expect(() => decoder(0)).toThrow();
+  expect(() => decoder(true)).toThrow();
+  expect(() => decoder(null)).toThrow();
+  expect(() => decoder(undefined)).toThrow();
+  expect(() => decoder([])).toThrow();
+  expect(() => decoder({})).toThrow();
 });
