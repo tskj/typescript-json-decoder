6cfe88900f43ca7fe4fea2c0c78ce6e172d6d019
Tarjei <epost@tarjei.org>
2022-05-05 22:03:23 +0200

    Add failing intersection of arrays of objects test

--- tests/unit.test.ts
+++ tests/unit.test.ts
@@ -611,6 +611,20 @@ test('intersection of arrays', () => {
   expect(() => intersect_decoder({ b: 0 })).toThrow()
 });
 
+/*
+test('intersection of arrays of objects', () => {
+  const test_value = [ { a: '0', b: 1 }, { a: '2', b: 3 } ];
+
+  type intersection = decodeType<typeof intersect_decoder>;
+  const intersect_decoder = intersection(array({ a: string }), array({ b: number }));
+
+  expect<intersection>(intersect_decoder([])).toEqual([]);
+  expect<intersection>(intersect_decoder(test_value)).toEqual(test_value);
+  expect(intersect_decoder(test_value).map(x => x.a)).toEqual([ '0', '2' ]);
+  expect(intersect_decoder(test_value).map(x => x.b)).toEqual([ 1, 3 ]);
+});
+*/
+
 test('intersection of tuple and array', () => {
   const test_value = [ 1, 2 ]
 
